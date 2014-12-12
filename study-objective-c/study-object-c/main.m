3e6f9f1751b1624fb4a39b652c1d640513e24c46
XiongZhijun <hust.xzj@gmail.com>
2014-12-12 21:40:53 +0800

    new project for study objective c

---
+++ study-objective-c/study-object-c/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  study-object-c
+//
+//  Created by 熊志军 on 12/12/14.
+//  Copyright (c) 2014 BobXiong. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
